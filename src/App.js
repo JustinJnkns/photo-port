5e62d36e7359ec6d1137ceb6d68507d09477fa16
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-12-24 02:12:49 -0500

    added an about page and index through react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// root component of application to effect change on application this file needs to be modified or have components added to it
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import About from './components/About'
+import Nav from "./components/Nav"
 
+// is JSX not HTML
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Nav></Nav>
+        <main>
+            <About></About>
+        </main>
     </div>
   );
 }
